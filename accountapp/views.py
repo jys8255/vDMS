c3c3f12f1eeff02f8bd2a109aaf5bcc4f1ebb395
JEONYOONSNG <yoonsung.jeon@adus.ai>
2024-03-29 17:01:51 +0900

    final

--- accountapp/views.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib.auth.views import LoginView
-from django.urls import reverse_lazy
-
-
-
-class CustomLoginView(LoginView):
-    template_name = 'accountapp/login.html'
-    success_url = reverse_lazy('registerapp:homepage')
